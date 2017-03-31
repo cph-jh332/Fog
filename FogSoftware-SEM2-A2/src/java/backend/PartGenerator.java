3084be3f1932792cef113e06383fe78985824ca3
minimekill <leonhardt_88@live.dk>
2017-03-31 09:55:24 +0200

    hul bånd

--- FogSoftware-SEM2-A2/src/java/backend/PartGenerator.java
+++ FogSoftware-SEM2-A2/src/java/backend/PartGenerator.java
@@ -17,8 +17,8 @@ public class PartGenerator
 
     public PartGenerator(int length, int width)
     {
-        carportLength = length*10;
-        carportWidth = width*10;
+        carportLength = length;
+        carportWidth = width;
     }
 
     // Length in millimeter //
