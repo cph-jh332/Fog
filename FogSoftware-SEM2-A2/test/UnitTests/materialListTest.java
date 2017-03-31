26040150bcc87f75e2a2555d77bb94e62361401b
JOACHIM-PC\Joachim <craciez@live.dk>
2017-03-31 11:49:12 +0200

    Added overtstern + shedboard + rem Tests

--- FogSoftware-SEM2-A2/test/UnitTests/materialListTest.java
+++ FogSoftware-SEM2-A2/test/UnitTests/materialListTest.java
@@ -33,12 +33,37 @@ public class materialListTest
     }
     
     @Test
-    public void understærnTest(){
+    public void understernTest(){
         int[] understern = pg.understernBrædder();
         int understern1 = understern[1];
         
         int expected = 4;
         assertEquals(expected, understern1);
+    }
+    
+    @Test
+    public void oversternTest(){
+        int[] overstern = pg.oversternBrædder();
+        int overstern1 = overstern[0];
+        
+        int expected = 2;
+        assertEquals(expected, overstern1);
+    }
+    
+    @Test
+    public void shedBoardTest(){
+        int shedBoard = pg.getShedBoards();
+        int expected = 200;
+        assertEquals(expected, shedBoard);
+    }
+    
+    @Test
+    public void remTest(){
+        int[] rem = pg.getRem();
+        int rem1 = rem[1];
+        
+        int expected = 2;
+        assertEquals(expected, rem1);
     }
 
 }
