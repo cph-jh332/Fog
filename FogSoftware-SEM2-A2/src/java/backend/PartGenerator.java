65aad02b7fdacfb398d5d355f8ab4685800a53d9
unknown <benjaminlrasmussen@gmail.com>
2017-03-31 10:52:35 +0200

    Length width change

--- FogSoftware-SEM2-A2/src/java/backend/PartGenerator.java
+++ FogSoftware-SEM2-A2/src/java/backend/PartGenerator.java
@@ -125,24 +125,24 @@ public class PartGenerator
     
     
 
-    public int[] understernBrædder(int længde, int bredde)
-    {
+    public int[] understernBrædder()
+    {   
         int[] understern = new int[2];
 
-        int temp360 = ((bredde / 360) + 1) * 2; //+1 because the costumer needs 1 extra for mistakes per length, *2 front and back!
-        int temp540 = ((længde / 540) + 1) * 2;
+        int temp360 = ((carportWidth / 360) + 1) * 2; //+1 because the costumer needs 1 extra for mistakes per length, *2 front and back!
+        int temp540 = ((carportLength / 540) + 1) * 2;
         understern[0] = temp360;
         understern[1] = temp540;
 
         return understern;
     }
 
-    public int[] oversternBrædder(int længde, int bredde)
+    public int[] oversternBrædder()
     {
         int[] overstern = new int[2];
 
-        int temp360 = ((bredde / 360) + 1); //no boards in the back 
-        int temp540 = ((længde / 540) + 1) * 2;
+        int temp360 = ((carportWidth / 360) + 1); //no boards in the back 
+        int temp540 = ((carportLength / 540) + 1) * 2;
         overstern[0] = temp360;
         overstern[1] = temp540;
 
