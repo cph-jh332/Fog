cdbe2a92efad793438b721872686e527f768edd6
minimekill <leonhardt_88@live.dk>
2017-04-19 17:22:11 +0200

    Backend to admin-projectpage added

--- FogSoftware-SEM2-A2/src/java/db/OrderMapper.java
+++ FogSoftware-SEM2-A2/src/java/db/OrderMapper.java
@@ -7,19 +7,33 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class OrderMapper {
 
-    public void storeOrder(User user, Order order) {
-        String sql = "INSERT INTO orders (userID, orderTitle) VALUES (?, ?);";
-
+    public void storeOrder(User user, Order order, ArrayList<String> materials) {
+        String sqlOrder = "INSERT INTO orders (userID, orderTitle) VALUES (?, ?);";
+        String sqlOrderMat = "INSERT INTO orderMaterials (material) VALUES (?);";
+        
+        String mats = String.join(",", materials);  //this converts the String ArrayList into one log String each element seperated by a " , "
+        
+        
+        
         try (Connection con = new DBConnector().getConnection()) {
-            PreparedStatement stmt = con.prepareStatement(sql);
+            PreparedStatement stmt = con.prepareStatement(sqlOrder);
             stmt.setInt(1, /*user.getId()*/ 666);
             stmt.setString(2, order.getOrder_title() + " - Carport med flat tag");
             stmt.executeUpdate();
+            
+            stmt = con.prepareStatement(sqlOrderMat);
+            stmt.setString(1, mats);
+            stmt.executeUpdate();
+            
+        
+  
+            
 
         } catch (SQLException ex) {
             Logger.getLogger(OrderMapper.class.getName()).log(Level.SEVERE, null, ex);
@@ -45,6 +59,32 @@ public class OrderMapper {
         }
 
         return orderList;
+    }
+    
+    public ArrayList getOrderMat(int orderNum){
+        String sql = "SELECT material FROM orderMaterials WHERE orderID = " + orderNum;
+        ArrayList<String> materials = new ArrayList<>();
+        
+                try (Connection con = new DBConnector().getConnection()) {
+                    PreparedStatement ps = con.prepareStatement(sql);
+                    ResultSet rs = ps.executeQuery();
+                    
+                    
+                        String material = rs.getString("material");      
+                        String[] material1 = material.split(",");
+                        
+                        materials = new ArrayList<>(Arrays.asList(material1));
+                        
+                        
+                        
+                    
+            
+        } catch (Exception ex) {
+            Logger.getLogger(UserMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+         return materials;
+        
     }
 
 }
--- FogSoftware-SEM2-A2/web/admin-projectpage.jsp
+++ FogSoftware-SEM2-A2/web/admin-projectpage.jsp
@@ -1,3 +1,4 @@
+<%@page import="java.util.ArrayList"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
 <!DOCTYPE html>
@@ -15,7 +16,41 @@
             <div class="row">
                 <h1>Carport project page</h1>
                 
-                
+                                <div class="table">
+                    <div class="table-title">
+                        <p>Stykliste til carport</p>
+                    </div>
+                    <div class="table-heading">
+                        <div class="table-cell">
+                            <p>Materiale</p>
+                        </div>
+                        <div class="table-cell">
+                            <p>Længde</p>
+                        </div>
+                        <div class="table-cell">
+                            <p>Antal</p>
+                        </div>
+                        <div class="table-cell">
+                            <p>Enhed</p>
+                        </div>
+                    </div>
+                    <%
+                        for (String str : (ArrayList<String>)request.getAttribute("materialList"))
+                        {
+                            String[] materialData = str.split("_");
+                            %> <div class="table-row"> <%
+                            for (String data : materialData)
+                            {
+                                %>
+                                <div class="table-cell">
+                                    <p><%out.println(data);%></p>
+                                </div>
+                                <%
+                            }
+                            %> </div> <%
+                        }
+                    %>
+                </div>
                 
             </div>
         </div>
