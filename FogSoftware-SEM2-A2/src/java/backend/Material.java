9df77a9665c185b6f966588c35f7bd45255623bb
minimekill <leonhardt_88@live.dk>
2017-04-20 11:42:33 +0200

    Material added!!!

---
+++ FogSoftware-SEM2-A2/src/java/backend/Material.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+/**
+ *
+ * @author Dixie
+ */
+public class Material {
+   private int ID,amount;
+   private String name;
+
+    public Material(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+   
+   
+   
+}
