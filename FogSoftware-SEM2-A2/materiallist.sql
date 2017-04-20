14fbebdd8c1a84504a87aeb407841ca2b0781c18
minimekill <leonhardt_88@live.dk>
2017-04-20 16:19:01 +0200

    added sql

---
+++ FogSoftware-SEM2-A2/materiallist.sql
@@ -0,0 +1,23 @@
+insert into materials (materialName) values ("45x195 mm. spærtræ ubh"); #rafter
+
+insert into materials (materialName) values ("97x97mm. trykimp. Stolpe"); #stolpe
+
+insert into materials (materialName) values ("Plastmo Ecolite blåtonet 600"); #tagplader
+insert into materials (materialName) values ("Plastmo Ecolite blåtonet 300");
+
+insert into materials (materialName) values ("45x195 mm. spærtræ ubh.600");
+insert into materials (materialName) values ("45x195 mm. spærtræ ubh.480");
+
+insert into materials (materialName) values ("19x100 mm. trykimp. Bræt 210"); #shedboards
+
+
+
+insert into materials (materialName) values ("25x200 mm. trykimp. Bræt 540"); #understern
+insert into materials (materialName) values ("25x200 mm. trykimp. Bræt 360");
+
+insert into materials (materialName) values ("25x125 mm. trykimp. Bræt 360"); #overstern
+insert into materials (materialName) values ("25x125 mm. trykimp. Bræt 360");
+
+
+insert into materials (materialName) values ("19x100 mm. trykimp. Bræt 540");
+insert into materials (materialName) values ("19x100 mm. trykimp. Bræt 360"); #vandbrædder
