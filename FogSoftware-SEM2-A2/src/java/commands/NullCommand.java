2967ff7a59e11a6fc58d57a15dcefc906cc3e9d9
Mikkel <mikkel@mediagram.dk>
2017-04-28 10:35:55 +0200

    Code cleanup

--- FogSoftware-SEM2-A2/src/java/commands/NullCommand.java
+++ FogSoftware-SEM2-A2/src/java/commands/NullCommand.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package commands;
 
 import interfaces.ICommand;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-/**
- *
- * @author Bade
- */
 public class NullCommand implements ICommand
 {
 
--- FogSoftware-SEM2-A2/src/java/commands/ViewOrderCommand.java
+++ FogSoftware-SEM2-A2/src/java/commands/ViewOrderCommand.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package commands;
 
 import db.OrderMapper;
@@ -10,10 +5,6 @@ import interfaces.ICommand;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-/**
- *
- * @author Bade
- */
 public class ViewOrderCommand implements ICommand
 {
 
