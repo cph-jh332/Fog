6b5c95f83c9d5d5c93e70cb6cf41166f53e8443a
JOACHIM-PC\craci <cph-jh332@cphbusiness.dk>
2017-05-01 10:23:25 +0200

    Home button in admin site is now fixed

---
+++ FogSoftware-SEM2-A2/src/java/commands/AdminHomePage.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commands;
+
+import backend.User;
+import db.OrderMapper;
+import interfaces.ICommand;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author craci
+ */
+public class AdminHomePage implements ICommand {
+
+    @Override
+    public RequestDispatcher execute(HttpServletRequest request) {
+        User currentUser = (User) request.getSession().getAttribute("user");
+        String admin = "admin.jsp";
+        String notAdmin = "index.jsp";
+        if (currentUser.isAdmin()) {
+            ArrayList orderList = new OrderMapper().getOrders("sqltop10");
+            request.setAttribute("list", orderList);
+            return request.getRequestDispatcher(admin);
+        }
+        return request.getRequestDispatcher(notAdmin);
+    }
+
+}
