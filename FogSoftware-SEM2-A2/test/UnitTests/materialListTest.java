c4d41d3906d1dfea859163813fd8ae134aebdecc
unknown <benjaminlrasmussen@gmail.com>
2017-05-05 09:31:46 +0200

    Cleanup

--- FogSoftware-SEM2-A2/test/UnitTests/materialListTest.java
+++ FogSoftware-SEM2-A2/test/UnitTests/materialListTest.java
@@ -1,18 +1,9 @@
 package UnitTests;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import backend.Material;
 import backend.PartGenerator;
 import db.DBFacade;
 import java.util.ArrayList;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
