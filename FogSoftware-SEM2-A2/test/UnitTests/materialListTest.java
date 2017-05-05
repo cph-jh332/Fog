31cc5187e1ff0f4534ce5923f30a09de3de8a70d
minimekill <leonhardt_88@live.dk>
2017-05-05 10:39:35 +0200

    Database test

--- FogSoftware-SEM2-A2/test/UnitTests/materialListTest.java
+++ FogSoftware-SEM2-A2/test/UnitTests/materialListTest.java
@@ -8,7 +8,13 @@ package UnitTests;
 import backend.Material;
 import backend.PartGenerator;
 import db.DBFacade;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,9 +23,59 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class materialListTest {
+
     DBFacade df = new DBFacade();
     PartGenerator pg = new PartGenerator(780, 600);
     Material material;
+    private final String USERNAME = "bade";
+    private final String PASSWORD = "3201";
+    private Connection conn = null;
+
+    @BeforeClass
+    public static void setUpClass() {
+
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+
+    }
+
+    @Before
+    public void setUp() {
+        String URL = String.format("jdbc:mysql://207.154.197.204:3306/FogDB?useUnicode=true&amp;characterEncoding=utf8");
+        try {
+            conn = (Connection) DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute("drop table if exists junittest");
+                stmt.execute("create table junittest as(select * from materials)");
+
+            }
+
+        } catch (SQLException ex) {
+            conn = null;
+            Logger.getLogger(materialListTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @After
+    public void tearDown() {
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.execute("SET foreign_key_checks = 0");
+            stmt.execute("drop table if exists materials");
+            stmt.execute("create table materials(materialID int auto_increment primary key, materialName varchar(100))");
+            stmt.execute("insert into materials(select * from junittest)");
+            stmt.execute("SET foreign_key_checks = 1");
+
+        } catch (SQLException ex) {
+            conn = null;
+            Logger.getLogger(materialListTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 
     @Test
     public void pillarsTest() {
@@ -119,31 +175,28 @@ public class materialListTest {
         assertEquals(lastExpected, lastMaterial);
 
     }
-    
+
     @Test
-    public void addAndDeleteMaterialsToAndFromDB(){
+    public void addAndDeleteMaterialsToAndFromDB() {
         String name = "et stykke bræt";
         material = new Material(name);
         df.addNewMaterial(material);
-        
+
         ArrayList<String> materials = df.getAllMaterials();
-        
-        
+
         String materialName = materials.get(materials.size() - 1);
         String expected = name;
-        
-        assertEquals(expected,materialName);
-        
+
+        assertEquals(expected, materialName);
+
         df.deleteMaterial(name);
-        
+
         materials = df.getAllMaterials();
-        
+
         materialName = materials.get(materials.size() - 1);
         String notExpected = name;
-        
-        assertNotEquals(notExpected,materialName);
-        
-    }
 
+        assertNotEquals(notExpected, materialName);
+    }
 
 }
