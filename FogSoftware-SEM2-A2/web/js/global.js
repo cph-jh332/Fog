c3a54cba10117bc82d3f8f9576b42dab8829605b
Mikkel <mikkel@mediagram.dk>
2017-05-05 10:51:29 +0200

    popups done

--- FogSoftware-SEM2-A2/web/js/global.js
+++ FogSoftware-SEM2-A2/web/js/global.js
@@ -1,8 +1,14 @@
 // Variables
 let popupLogin = document.getElementById("login-popup");
-let popupSignup = document.getElementById("signup-popup")
+let popupSignup = document.getElementById("signup-popup");
 let loginButton = document.getElementById("login-button");
 let signupButton = document.getElementById("signup-button");
+let popupContainer = document.getElementsByClassName("popup-container");
+let popupContent = document.getElementsByClassName("popup-content");
+
+for (let i = 0; i < popupContent.length; i++) {
+    popupContent[i].insertAdjacentHTML("afterbegin", "<img src='img/close-button.png' class='close-popup' id='close-popup'>");
+}
 
 // Functions
 function showLogin() {
@@ -13,6 +19,19 @@ function showSignup() {
     popupSignup.style.display = "block";
 }
 
-// Click events
+function removePopup(e) {
+    for (let i = 0; i < popupContainer.length; i++) {
+        popupContainer[i].style.display = "none";
+        e.stopPropagation();
+    }
+}
+
+// Click events 
 loginButton.addEventListener("click", showLogin);
 signupButton.addEventListener("click", showSignup);
+
+let closePopup = document.getElementsByClassName("close-popup");
+for (let i = 0; i < closePopup.length; i++) {
+    closePopup[i].addEventListener("click", removePopup, false);
+}
+
