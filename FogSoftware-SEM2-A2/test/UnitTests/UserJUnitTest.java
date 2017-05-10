b2f97508c4e664928b9fcfea923efa465225b5d1
Mikkel <mikkel@mediagram.dk>
2017-05-10 09:48:51 +0200

    code cleanup

--- FogSoftware-SEM2-A2/test/UnitTests/UserJUnitTest.java
+++ FogSoftware-SEM2-A2/test/UnitTests/UserJUnitTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package UnitTests;
 
 import backend.User;
@@ -14,10 +9,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Dixie
- */
 public class UserJUnitTest {
 
     DBFacade userMapper = new DBFacade();
