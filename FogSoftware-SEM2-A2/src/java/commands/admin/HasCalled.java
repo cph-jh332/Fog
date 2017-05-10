85616fcfbc638f7bfee43fc179d88c4e3a3d09b3
Mikkel <mikkel@mediagram.dk>
2017-05-10 09:36:04 +0200

    code cleanup

--- FogSoftware-SEM2-A2/src/java/commands/admin/HasCalled.java
+++ FogSoftware-SEM2-A2/src/java/commands/admin/HasCalled.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package commands.admin;
 
 import backend.Material;
@@ -16,10 +11,6 @@ import java.util.HashMap;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-/**
- *
- * @author craci
- */
 public class HasCalled implements ICommand {
 
     DBFacade df = new DBFacade();
